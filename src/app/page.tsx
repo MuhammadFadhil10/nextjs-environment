68ee41b284ad00de284cbd7f9caeded6460ac67b
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-09-23 14:46:10 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Next js environment</h1>
+
+      <h1>this is {process.env.NODE_ENV} environment</h1>
     </main>
   );
 }
